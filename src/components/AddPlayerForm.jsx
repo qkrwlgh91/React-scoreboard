8366408da1dff0f4776e338bf1de7f0d13d5e736
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-10 23:58:03 +0900

    added html5 validation

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -5,12 +5,28 @@ class AddPlayerForm extends Component {
         value: ''
     }
 
+    constructor() {
+        super();
+        this.formRef = React.createRef();
+        this.textRef = React.createRef();
+    }
+
     handleValueChange = (e) => {
         this.setState({value: e.target.value})
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
+
+        const form = this.formRef.current; // form node
+        //const player = this.textRef.current; // input node
+
+        console.log(form.checkValidity());
+        //console.log(player.validity.valid);
+
+        if (!form.checkValidity()) {
+            return;
+        }
         this.props.addPlayer(this.state.value);
         this.setState({
             value: ''
@@ -19,9 +35,9 @@ class AddPlayerForm extends Component {
 
     render() {
         return (
-            <form className="form" onSubmit={this.handleSubmit}>
+            <form ref={this.formRef} className="form" onSubmit={this.handleSubmit} noValidate>
                 <input className="input" type="text" placeholder="enter a player's name"
-                value={this.state.value} onChange={this.handleValueChange}></input>
+                value={this.state.value} onChange={this.handleValueChange} required></input>
                 <input className="input" type="submit" value="Add Player"></input>
             </form>
         )
